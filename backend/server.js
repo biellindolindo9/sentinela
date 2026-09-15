88781cb4ab5d29974ca1896be4684290e97ee492
Gabriel Vitor <167567983+biellindolindo9@users.noreply.github.com>
2026-09-15 10:11:08 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-```js
+
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
@@ -363,4 +363,3 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`🏥 Hospital Pro rodando na porta ${PORT}`);
 });
-```
